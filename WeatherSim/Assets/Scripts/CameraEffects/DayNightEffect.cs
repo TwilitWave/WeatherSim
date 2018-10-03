cb14ccf091f400242bcdf0e0e97afae1d4eea447
Andrew Taylor Johnson <andrewtaylorjohnson@gmail.com>
2018-10-03 13:41:00 -0600

    Created day and night cycle shader. Switched to different style of paper for post effect.

---
+++ WeatherSim/Assets/Scripts/CameraEffects/DayNightEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DayNightEffect : MonoBehaviour
+{
+    private Material DayNightMaterial;
+
+    public Color DayColor;
+    public Color NightColor;
+    [Range(0, 1)]
+    public float SunPosition;
+
+    private void Awake()
+    {
+        DayNightMaterial = new Material(Shader.Find("Hidden/DayNightEffect"));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, DayNightMaterial);
+    }
+
+    private void Update()
+    {
+        DayNightMaterial.SetFloat("_SunPosition", SunPosition);
+        DayNightMaterial.SetColor("_DayColor", DayColor);
+        DayNightMaterial.SetColor("_NightColor", NightColor);
+    }
+}
