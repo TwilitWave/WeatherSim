22f740b70d44d6c8b03a21dee61e79c261af3e13
AN ZHU <zhua@kean.edu>
2018-10-03 21:14:38 -0600

    Version 1.0

---
+++ WeatherSim/Assets/Scripts/Manager/ReStartManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReStartManager : MonoBehaviour
+{
+    private Button my_Button;
+    // Use this for initialization
+    void Start()
+    {
+        my_Button = this.GetComponent<Button>();
+        my_Button.onClick.AddListener(Click);
+    }
+
+    // Update is called once per frame
+    void Click()
+    {
+        SceneManager.LoadScene("Main 1", LoadSceneMode.Single);
+    }
+
+}
